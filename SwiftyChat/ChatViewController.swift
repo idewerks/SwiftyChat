23ebf045bec01705fb79d11cd7e344e2177f8641
IDEWERKS <idewerks@gmail.com>
2016-08-03 22:06:21 -0500

    Integration of JSQmessagesViewController with Login VC. Added logout button to conversations list- functional but not ideal

--- SwiftyChat/ChatViewController.swift
+++ SwiftyChat/ChatViewController.swift
@@ -1,61 +1,91 @@
 //
 //  ChatViewController.swift
-//  SwiftyChat
+//  SwiftExample
 //
-//  Created by kevin brennan on 7/31/16.
-//  Copyright © 2016 kevin brennan. All rights reserved.
+//  Created by Dan Leonard on 5/11/16.
+//  Copyright © 2016 MacMeDan. All rights reserved.
 //
 
 import UIKit
-import Firebase
 import JSQMessagesViewController
 
-
-
-class ChatViewController: UIViewController {
-
+class ChatViewController: JSQMessagesViewController {
+    var messages = [JSQMessage]()
+    
+    var conversation: Conversation?
+    let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleBlueColor())
+    let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImageWithColor(UIColor.lightGrayColor())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.inputToolbar?.contentView?.leftBarButtonItem = nil
+        
+        
+        // This is how you remove Avatars from the messagesView
+        collectionView?.collectionViewLayout.incomingAvatarViewSize = .zero
+        collectionView?.collectionViewLayout.outgoingAvatarViewSize = .zero
+        
+        // This is a beta feature that mostly works but to make things more stable I have diabled it.
+        collectionView?.collectionViewLayout.springinessEnabled = false
+        
+        //Set the SenderId  to the current User
+        // For this Demo we will use Woz's ID
+        // Anywhere that AvatarIDWoz is used you should replace with you currentUserVariable
+        senderId = AvatarIdWoz
+        senderDisplayName = conversation?.firstName ?? conversation?.preferredName ?? conversation?.lastName ?? ""
+        automaticallyScrollsToMostRecentMessage = true
+        
+        if (conversation?.smsNumber) != nil {
+            self.messages = makeConversation()
+            self.collectionView?.reloadData()
+            self.collectionView?.layoutIfNeeded()
+        }
     }
-
-  override func viewWillAppear(animated: Bool) {
     
-  }
-  
-  
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-  @IBAction func signOut(sender: UIButton) {
-
-    
-    let firebaseAuth = FIRAuth.auth()
-    do {
-      try firebaseAuth?.signOut()
-      AppState.sharedInstance.signedIn = false
-      performSegueWithIdentifier(Constants.Segues.ChatToSignIn, sender: nil)
-    } catch let signOutError as NSError {
-      print ("Error signing out: \(signOutError)")
+    override func didPressSendButton(button: UIButton?, withMessageText text: String?, senderId: String?, senderDisplayName: String?, date: NSDate?) {
+        
+        // This is where you would impliment your method for saving the message to your backend.
+        //
+        // For this Demo I will just add it to the messages list localy
+        //
+        self.messages.append(JSQMessage(senderId: AvatarIdWoz, displayName: DisplayNameWoz, text: text))
+        self.finishSendingMessageAnimated(true)
+        self.collectionView?.reloadData()
     }
     
-    
-    
-    
-    
-    
-  }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
     }
-    */
-
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView?, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData? {
+        return messages[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView?, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource? {
+        return messages[indexPath.item].senderId == AvatarIdWoz ? outgoingBubble : incomingBubble
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource? {
+        return nil
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView?, attributedTextForMessageBubbleTopLabelAtIndexPath indexPath: NSIndexPath!) -> NSAttributedString! {
+        let message = messages[indexPath.item]
+        switch message.senderId {
+        case AvatarIdWoz:
+            return nil
+        default:
+            guard let senderDisplayName = message.senderDisplayName else {
+                assertionFailure()
+                return nil
+            }
+            return NSAttributedString(string: senderDisplayName)
+            
+        }
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView?, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout?, heightForMessageBubbleTopLabelAtIndexPath indexPath: NSIndexPath!) -> CGFloat {
+        return messages[indexPath.item].senderId == AvatarIdWoz ? 0 : kJSQMessagesCollectionViewCellLabelHeightDefault
+    }
+    
 }
